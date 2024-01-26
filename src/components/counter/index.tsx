ca9b27ea1eaadae923283c0bc8c726d0c1fa08da
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-01-26 19:05:06 -0300

    :sparkles: feat: Component Counter: pega o valor de count na loja useCounterStore e render em h2

---
+++ src/components/counter/index.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useCounterStore } from '@/hooks/store'
+
+const Counter = () => {
+  //observando somente o estado count, se outros estados forem alterado, esse componente não é renderizado novamente.
+  const counter = useCounterStore((stateCounter) => stateCounter.count)
+  return (
+    <div>
+      <h2 className="font-mono text-xl font-bold">{counter}</h2>
+    </div>
+  )
+}
+
+export default Counter
