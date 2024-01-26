935c8025a8636967f5c26f5715c8b8213cb0e00d
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-01-26 20:05:37 -0300

    :sparkles: feat: Mostrando o formulario e seu nome, sobrenome na página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-import Counter from '@/components/counter'
-import {
-  ButtonDescrementCounter,
-  ButtonIncrementCounter,
-  ButtonResetCounter
-} from '@/components/counter/buttons-counter'
+import ExampleUpdatingState from '@/components/examples/updating-state'
+import PersonName from '@/components/examples/updating-state/person-name'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Counter />
-      <h3>Ações</h3>
-      <div className="flex gap-8">
-        <ButtonIncrementCounter />
-        <ButtonDescrementCounter />
-      </div>
-      <ButtonResetCounter />
+      <PersonName />
+      <ExampleUpdatingState />
     </main>
   )
 }
