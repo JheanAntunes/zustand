169bee0adb25491abd9ba848358455834bc1be8b
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-01-26 20:01:32 -0300

    :sparkles: feat: hook usePersonStore: gerenciamento de nome e sobrenome

---
+++ src/hooks/usePersonStore.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+
+type State = {
+  firstName: string
+  lastName: string
+}
+
+type Actions = {
+  updatingFirstName: (newFirstName: State['firstName']) => void
+  updatingLastName: (newLastName: State['lastName']) => void
+}
+
+type UsePersonStore = State & Actions
+
+export const usePersonStore = create<UsePersonStore>((set) => ({
+  firstName: '',
+  lastName: '',
+  updatingFirstName: (newFirstName) => set(() => ({ firstName: newFirstName })),
+  updatingLastName: (newLastName) => set(() => ({ lastName: newLastName }))
+}))
