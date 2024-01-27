cbe02a35a91e7027eb2a87c9f41cf81569b395fc
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-01-26 21:24:47 -0300

    :sparkles: feat: Component Página home: import o Component DeeplyNestedObject exemplo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import ExampleUpdatingState from '@/components/examples/updating-state'
-import PersonName from '@/components/examples/updating-state/person-name'
+import DeeplyNestedObject from '@/components/examples/deeply-nested-object'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <PersonName />
-      <ExampleUpdatingState />
+      <DeeplyNestedObject />
     </main>
   )
 }
