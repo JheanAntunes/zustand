644b208cfa1bf3e9520a56e9bc6116b901f7ff97
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-01-27 20:39:03 -0300

    :wrench: chore: jest

---
+++ src/jest.config.ts
@@ -0,0 +1,10 @@
+// jest.config.ts
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./src/setup-jest.ts'],
+}
+
+export default config
