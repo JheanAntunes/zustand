8c448004200f93f5ce15e9413553b73ffbbe9ac3
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-01-29 20:27:05 -0300

    :sparkles: feat: Component MailDisplay:  add feature delete e archives em progresso

--- src/mail/use-mail.ts
+++ src/mail/use-mail.ts
@@ -5,16 +5,42 @@ import { Mail, mails } from '@/mail/data'
 
 type Config = {
   selected: Mail['id'] | null
+  mails: Mail[]
+  trash: Mail[]
+  archives: Mail[]
 }
 
 type Actions = {
   setSelected: (selectedId: string) => void
+  deleteMail: (selectedId: string) => void
+  setArchive: (selectedId: string) => void
 }
 
 const configCreator: StateCreator<Config & Actions> = (set) => ({
+  mails,
+  trash: [],
+  archives: [],
   selected: mails[0].id,
   setSelected: (selectedId: string) => {
     set(() => ({ selected: selectedId }))
+  },
+  deleteMail: (selectedId: string) => {
+    set((state) => ({
+      trash: [
+        ...state.trash,
+        state.mails.find((mail) => mail.id === selectedId)!
+      ],
+      mails: state.mails.filter((mail) => mail.id !== selectedId)
+    }))
+  },
+  setArchive: (selectedId: string) => {
+    set((state) => ({
+      archives: [
+        ...state.archives,
+        state.mails.find((mail) => mail.id === selectedId)!
+      ],
+      mails: state.mails.filter((mail) => mail.id !== selectedId)
+    }))
   }
 })
 
@@ -25,7 +51,10 @@ export const useMailStore = create<Config & Actions>()(configCreator)
 // }
 
 const configAtom = atom<Config>({
-  selected: mails[0].id
+  selected: mails[0].id,
+  mails,
+  trash: [],
+  archives: []
 })
 
 export function useMail() {
