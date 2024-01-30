e75685e38fe1a5109dd9dad9ea43489fd3057205
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-01-29 23:44:59 -0300

    :sparkles: feat: ButtonTrash: mover email para lixeira

---
+++ src/mail/trash/button-trash.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
+import { Mail } from '@/mail/data'
+import { Trash2 } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import { useMailStore } from '../use-mail'
+
+interface ButtonTrashProps {
+  mail: Mail | null
+}
+
+const ButtonTrash = ({ mail }: ButtonTrashProps) => {
+  const mailTrash = useMailStore((state) => state.trash)
+  const setDeleteMail = useMailStore((state) => state.deleteMail)
+  const pathname = usePathname()
+  const type = pathname === '/archives' ? 'archives' : 'mail'
+  const isFindMailTrash = () => {
+    return mailTrash.find((trash) => trash.id === mail?.id) ? true : false
+  }
+
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          disabled={!mail || isFindMailTrash()}
+          onClick={() => {
+            if (mail && !isFindMailTrash()) {
+              setDeleteMail(type, mail.id)
+            }
+          }}
+        >
+          <Trash2 className="h-4 w-4" />
+          <span className="sr-only">Move to trash</span>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>Move to trash</TooltipContent>
+    </Tooltip>
+  )
+}
+
+export default ButtonTrash
