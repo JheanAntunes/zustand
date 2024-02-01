abd2660ad8728e9b98d86c9a0a94e3be00c63c72
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-01 19:17:18 -0300

    :books: docs: remover images comentadas

--- src/mail/mail-page.tsx
+++ src/mail/mail-page.tsx
@@ -13,22 +13,6 @@ export default function MailPage({ search }: Props) {
 
   return (
     <>
-      <div className="md:hidden">
-        {/* <Image
-          src="/examples/mail-dark.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="hidden dark:block"
-        />
-        <Image
-          src="/examples/mail-light.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="block dark:hidden"
-        /> */}
-      </div>
       <div className="hidden flex-col md:flex">
         <WrapperDataMail
           defaultLayout={defaultLayout}
