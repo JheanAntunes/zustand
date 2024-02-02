9aaa15cd3ae78240c026d496da0c54d974856ea5
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-01 21:03:52 -0300

    💅  styles: eslint-prettier

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -4,7 +4,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./src/setup-jest.ts'],
+  setupFilesAfterEnv: ['./src/setup-jest.ts']
 }
 
 export default config
